46ec662e3e3d4923cdae47b7044c1c05f40a686d
Vika201 <Z.Vikysja95@gmail.com>
2021-03-05 18:29:02 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+
+const App = () => {
+
+    const todoDate = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awersome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ]
+
+    return ( <
+        div > <
+        AppHeader / >
+        <
+        SearchPanel / >
+
+        <
+        TodoList todos = { todoDate }
+        / > < /
+        div >
+    );
+};
+
+ReactDOM.render( < App / > ,
+    document.getElementById('root'));
