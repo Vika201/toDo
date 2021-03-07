b35ef10c48ed5b8a2bd4042eda5cd29e982447f3
Vika201 <Z.Vikysja95@gmail.com>
2021-03-07 23:49:14 +0200

    Structure of project

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from "../item-status-filter";
+import './index.css';
+
+const App = () => {
+
+    const todoDate = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awersome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ]
+
+    return (
+        <div className="todo-app container-fluid">
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel/>
+                <ItemStatusFilter/>
+            </div>
+            <TodoList className="i-todo-list"
+                      todos = { todoDate }/>
+        </div >
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from "./components/item-status-filter";
-
-const App = () => {
-
-    const todoDate = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awersome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ]
-
-    return (
-        <div>
-            <AppHeader/>
-            <div>
-                <SearchPanel/>
-                <ItemStatusFilter/>
-            </div>
-            <TodoList todos = { todoDate }/>
-        </div >
-    );
-};
+import App from './components/app';
 
 ReactDOM.render( <App/> ,
     document.getElementById('root'));
