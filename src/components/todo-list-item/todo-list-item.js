8060616e94fa972de601c11ca3ba72a359625794
Vika201 <Z.Vikysja95@gmail.com>
2021-03-08 22:33:57 +0200

    Stateful components

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
+
+    render() {
+
+        const { label, important = false } = this.props;
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -10,7 +14,7 @@ const TodoListItem = ({ label, important = false }) => {
         }
 
         return (
-        <span className="todo-list-item">
+            <span className="todo-list-item">
             < span className = "todo-list-item-label"
                    style = { style } >
                     { label }
@@ -27,6 +31,5 @@ const TodoListItem = ({ label, important = false }) => {
             </button>
         </span>
         );
+    };
 };
-
-        export default TodoListItem;
